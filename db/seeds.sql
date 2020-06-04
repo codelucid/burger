3487f2f2d8917f7ea0aafe74c29f6f7568931719
Coop <61427314+codelucid@users.noreply.github.com>
2020-06-04 15:00:52 -0400

    created connection.js, orm.js, schema.sql, seeds.sql, and server.js. Need more on server.js

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+("Triple Deluxe", false),
+("Single Thin", false),
+("Beef Fight", true),
+("Lettuce Wrap", false),
+("The Fav", true);
